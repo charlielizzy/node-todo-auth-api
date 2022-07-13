f1ca567e502346e9d0cd0918261cc41b044a0a60
Charlotte Walker <charlielizzy@me.com>
2022-07-13 20:06:05 +0100

    can access own todos on auth

--- index.js
+++ index.js
@@ -1,31 +1,50 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const app = express()
-const port = 3001
-const dotenv = require('dotenv');
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+const port = 3001;
+const dotenv = require("dotenv");
 dotenv.config();
-const db = require('./queries')
-const cors = require('cors');
+const db = require("./queries");
+const cors = require("cors");
+const { expressjwt: jwt } = require("express-jwt");
+const jwksRsa = require("jwks-rsa");
 
-app.use(bodyParser.json())
+const checkJwt = jwt({
+  // Dynamically provide a signing key based on the kid in the header and the signing keys provided by the JWKS endpoint
+  secret: jwksRsa.expressJwtSecret({
+    cache: true,
+    rateLimit: true,
+    jwksRequestsPerMinute: 5,
+    jwksUri: `https://paddington.eu.auth0.com/.well-known/jwks.json`,
+  }),
+
+  // Validate the audience and the issuer
+  audience: "https://paddington.eu.auth0.com/api/v2/", //replace with your API's audience, available at Dashboard > APIs
+  issuer: "https://paddington.eu.auth0.com/",
+  algorithms: ["RS256"],
+});
+
+app.use(bodyParser.json());
 app.use(
   bodyParser.urlencoded({
     extended: true,
   })
 );
 
-app.use(cors({
-  origin: '*'
-}));
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 
-app.get('/', (request, response) => {
-  response.json({ info: 'Hello World' })
-})
+app.get("/", (request, response) => {
+  response.json({ info: "Hello World" });
+});
 
-app.get('/users/:id/todos', db.getTodos)
+app.get("/todos", checkJwt, db.getTodos);
 
-app.post('/todos', db.createTodos)
+app.post("/todos", db.createTodos);
 
 app.listen(port, () => {
-  console.log(`Todo List API started ${port}.`)
-})
+  console.log(`Todo List API started ${port}.`);
+});
--- queries.js
+++ queries.js
@@ -1,37 +1,48 @@
-const Pool = require('pg').Pool
+const Pool = require("pg").Pool;
+const jwt_decode = require("jwt-decode");
 
-const pool = new Pool({  
+const pool = new Pool({
   host: process.env.HEROKU_DB_HOST,
-  database: process.env.HEROKU_DB_NAME,  
+  database: process.env.HEROKU_DB_NAME,
   port: process.env.HEROKU_DB_PORT,
   user: process.env.HEROKU_DB_USER,
-  password: process.env.HEROKU_DB_PASSWORD,  
+  password: process.env.HEROKU_DB_PASSWORD,
   ssl: {
     rejectUnauthorized: false,
   },
-})
+});
 
-const getTodos = async(request, response) => {
-  const id = parseInt(request.params.id);
-  pool.query('SELECT * FROM todos WHERE user_id = $1', [id], (error, results) => {
-    if (error) {
-      throw error
-    }
-    response.status(200).json(results.rows)
-  })
-}
+const getTodos = async (request, response) => {
+  const { authorization } = request.headers;
+  const decoded = jwt_decode(authorization);
+  const { sub } = decoded;
+  const userResults = await pool.query(
+    "SELECT id FROM users WHERE auth_0_id = $1",
+    [sub]
+  );
+  const { id: userid } = userResults.rows[0];
 
-const createTodos = async(request, response, next) => {
+  const todoResults = await pool.query(
+    "SELECT * FROM todos WHERE user_id = $1",
+    [userid]
+  );
+  response.status(200).json(todoResults.rows);
+};
+
+const createTodos = async (request, response, next) => {
   try {
-    const { task, user_id } = request.body    
-    const todo = await pool.query('INSERT INTO todos (user_id, is_complete, task) VALUES  ($1, $2, $3)', [user_id, false, task])
+    const { task, user_id } = request.body;
+    const todo = await pool.query(
+      "INSERT INTO todos (user_id, is_complete, task) VALUES  ($1, $2, $3)",
+      [user_id, false, task]
+    );
     response.status(200).json(todo.rows[0]);
-  } catch(error) {
+  } catch (error) {
     next(error);
   }
-}
+};
 
 module.exports = {
   getTodos,
-  createTodos
-}
+  createTodos,
+};
